3c608552ab5493ee7cf17780f4ee270762d62d5c
Isachi25 <sharonachiengmasiga@gmail.com>
2024-07-18 23:41:43 +0300

    Correct syntax

--- reservations/views.py
+++ reservations/views.py
@@ -16,6 +16,12 @@ from django.utils.dateparse import parse_datetime
 def about(request):
     return render(request, 'about.html')
 
+def services(request):
+    return render(request, 'services.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
+
 def home(request):
     return render(request, 'reservations/index.html')
 
