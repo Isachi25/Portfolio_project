20f8721c34afd6a063ef1b6e7d731e84a15bf816
Isachi25 <sharonachiengmasiga@gmail.com>
2024-07-18 23:01:44 +0300

    Correcting views.py

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import home, register, login_view, dashboard, create_reservation, parking_spots_view, reserve_spot_view, reservation_success_view, mpesa_callback, verify_and_reserve, calculate_payment_amount, check_availability
+from . import views
 
 urlpatterns = [
     path('', home, name='home'),
